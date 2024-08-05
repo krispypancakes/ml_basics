24c195fe12ba91fce904b76a9eb111500fa2789a
krispypancakes <jvfqm@pm.me>
2024-08-05 21:16:24 +0200

    working on batch implementation

--- python_experiments/nn_opt.py
+++ python_experiments/nn_opt.py
@@ -2,12 +2,6 @@
 
 import numpy as np
 
-N_INPUT = 784
-N_HIDDEN_1 = 128
-# N_HIDDEN_2 = 20
-N_OUT = 10
-LR = 0.001
-
 
 def flatten(_imgs: np.ndarray) -> np.ndarray:
     flattened = [img.flatten() for img in _imgs]
@@ -24,37 +18,36 @@ def hidden_layer(_input: np.ndarray, weights_mat: np.ndarray, bias: np.ndarray) 
 	return z
 
 
-def onehot(label):
-    vec = np.zeros(N_OUT)
-    vec[label] = 1
-    return vec
-
+def onehot(labels: np.ndarray, n_classes: int = 10):
+    onehot = np.zeros((labels.size, n_classes))
+    onehot[np.arange(labels.size), labels] = 1
+    return onehot
 
 def softmax(preds: np.ndarray) -> np.ndarray:
-    max_pred = max(preds)
+    max_pred = np.max(preds, axis=1, keepdims=True)
     exp_preds = np.exp(preds - max_pred)
-    return exp_preds / exp_preds.sum(axis=0)
+    return exp_preds / np.sum(exp_preds, axis=1, keepdims=True)
 
 
 def cross_entropy_loss(pred: np.ndarray, target: np.ndarray) -> np.ndarray:
     eps = 1e-15
     pred = np.clip(pred, eps, 1-eps) 
-    L = -np.sum(target * np.log(pred))
-    return L
+    L = -np.sum(target * np.log(pred), axis=1)
+    return np.mean(L)
 
 
-def backward_out(logits: np.ndarray, label: np.ndarray, activations: np.ndarray) -> tuple[np.ndarray]:
-    dz = logits - label
-    dw = np.outer(activations, dz)
-    db = dz
-    return dw, db
+def backward_out(logits: np.ndarray, labels: np.ndarray, activations: np.ndarray) -> tuple[np.ndarray]:
+    dz = logits - labels
+    dw = activations.T @ dz / labels.shape[0]
+    db = np.mean(dz, axis=0)
+    return dw, db, dz
     
 
 def backward_hidden(inputs: np.ndarray, weights_next: np.ndarray, dz_next: np.ndarray) -> tuple[np.ndarray]:
-    da = dz_next @ np.transpose(weights_next)
-    dz = np.where(da > 0, da, 0)
-    dw = np.outer(inputs, dz)
-    db = dz
+    da = dz_next @ weights_next.T
+    dz = np.where(da>0, da, 0)
+    dw = inputs.T @ dz / inputs.shape[0]
+    db = np.mean(dz, axis=0)
     return dw, db
 
 
